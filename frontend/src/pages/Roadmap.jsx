fa6dd036b45ebc2723a65f9804516f4b14e4533a
Nicole le Fevre <83703145+nicolelefevre98@users.noreply.github.com>
2023-12-18 22:54:35 +0100

    roadmap

--- frontend/src/pages/Roadmap.jsx
+++ frontend/src/pages/Roadmap.jsx
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import  '/./src/styles/Roadmap.scss';
-import RoadmapColumn from '../components/Roadmapcolumn';
+import RoadmapColumn from '../components/RoadmapColumn';
 import { Link } from 'react-router-dom';
 
 const Roadmap = () => {
